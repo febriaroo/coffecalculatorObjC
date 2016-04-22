f0fdfb34d87c8d78625546a0c9d64093cbf27865
febriaroo <febriaroosita@gmail.com>
2016-04-23 00:29:32 +0700

    Initial Commit

---
+++ Coffeecalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coffeecalculator
+//
+//  Created by Febria Roosita Dwi on 4/23/16.
+//  Copyright © 2016 febriaroosita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Coffeecalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Coffeecalculator
+//
+//  Created by Febria Roosita Dwi on 4/23/16.
+//  Copyright © 2016 febriaroosita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
